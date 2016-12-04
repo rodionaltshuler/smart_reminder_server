a15e26759e9dfc59dacc9862f48fdb956e6a70b9
Rodion Altshuler <rodion.altshuler@gmail.com>
2016-12-04 18:41:28 +0200

    itemLists: POST and GET routes

--- api.js
+++ api.js
@@ -71,16 +71,58 @@ module.exports = function (wagner) {
         }
     }));
 
-    api.get('/me', function(req, res) {
+    api.get('/me', function (req, res) {
         console.log('Session: ' + JSON.stringify(req.session));
         if (!req.user) {
-            return res.
-            status(status.UNAUTHORIZED).
-            json({ error: 'Not logged in' });
+            return res.status(status.UNAUTHORIZED).json({error: 'Not logged in'});
         }
         res.send(req.user);
 
     });
+
+    api.post('/itemLists', wagner.invoke(function (ItemList) {
+        return function (req, res) {
+
+            if (!req.user) {
+                return res.status(status.UNAUTHORIZED)
+                    .json({error: 'Not logged in'});
+            } else {
+
+                console.log('Creating items list: ' + req.user);
+
+                var list = new ItemList({name: req.body.name});
+                ItemList.findOne({name: list.name, collaboratingUsers: req.user._id}, function (err, existingList) {
+                    //user with this email already exists
+                    if (existingList) {
+                        return res.status(status.CONFLICT).json({error: 'ItemList with this name already exists for a user: ' + list.name});
+                    }
+                    list.collaboratingUsers.push(req.user._id);
+                    list.save(function (error, list) {
+                        if (error) {
+                            return res.status(status.INTERNAL_SERVER_ERROR).json({error: 'Cannot create itemList: ' + error.toString()});
+                        }
+                        res.send(list);
+                    });
+                });
+            }
+        }
+    }));
+
+    api.get('/itemLists', wagner.invoke(function (ItemList) {
+       return function (req, res) {
+           if (!req.user) {
+               return res.status(status.UNAUTHORIZED)
+                   .json({error: 'Not logged in'});
+           } else {
+               ItemList.find({collaboratingUsers: req.user._id}, function (error, lists) {
+                   if (error) {
+                       return res.status(status.INTERNAL_SERVER_ERROR).json({error: 'Cannot get item lists: ' + error.toString()});
+                   }
+                   res.send(lists);
+               });
+           }
+       }
+    }));
 
     return api;
 
