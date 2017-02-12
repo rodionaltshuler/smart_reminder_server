9cf6919a2e04833f4d23b59964a23acbd4432148
Rodion Altshuler <rodion.altshuler@gmail.com>
2017-02-13 00:58:27 +0200

    Use process.env.FB_APP_ID in config

--- fbconfig.js
+++ fbconfig.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let config = {
-    clientId: '1203169209731550',
+    clientId: process.env.FB_APP_ID || '1633896273294056',
     secret: process.env.FB_SECRET || 'put-fb-secret-here'
 };
 
